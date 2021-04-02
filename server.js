ebfae264cd0daa6d3e0824b06b61ad14cc4630e4
Petr <solodov.petr@gmail.com>
2021-04-02 15:35:35 +0300

    Response with JSON

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const http = require('http');
-const fs = require('fs');
-
-// Read files
-const HTML = fs.readFileSync(`${__dirname}/index.html`);
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const colors = ['red', 'green', 'blue'];
+const car = { brand: 'Ford', model: 'Fiesta', year: 2021 };
+
 const server = http.createServer( (req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Type', 'application/json');
+
+    const json = JSON.stringify({
+        colors,
+        car
+    });
+
     // Close connection, send response
-    res.end(HTML);
+    res.end(json);
 });
 
 server.listen(port, hostname, () => {
