112d6b94f09be96832de43f41245cb0219e63600
Petr <solodov.petr@gmail.com>
2021-04-02 16:01:36 +0300

    Routes basic example

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 const http = require('http');
+const fs = require('fs');
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const colors = ['red', 'green', 'blue'];
 const car = { brand: 'Ford', model: 'Fiesta', year: 2021 };
+const HTML = fs.readFileSync(`${__dirname}/index.html`);
 
 const server = http.createServer( (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
 
-    const json = JSON.stringify({
-        colors,
-        car
-    });
+    if ( req.url === '/' ) {
+        res.writeHead(200,{ 'Content-type':'text/html'})
+        res.end(HTML)
+    } else if ( req.url === '/api/car') {
+        res.writeHead(200,{ 'Content-type':'application/json'})
+        const json = JSON.stringify( car )
+        res.end(json)
+    } else if ( req.url === '/api/colors') {
+        res.writeHead(200,{ 'Content-type':'application/json'})
+        const json = JSON.stringify( colors )
+        res.end(json)
+    }
 
-    // Close connection, send response
-    res.end(json);
+    // 404 response
+    res.writeHead(404);
+    res.end();
+
 });
 
 server.listen(port, hostname, () => {
