17d890951df906d2dd2893786672f1e4709bde53
Petr <solodov.petr@gmail.com>
2021-04-02 14:56:33 +0300

    Response with HTML

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const http = require('http');
+const fs = require('fs');
+
+// Read files
+const HTML = fs.readFileSync(`${__dirname}/index.html`);
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer( (req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html');
     // Close connection, send response
-    res.end('Hello World');
+    res.end(HTML);
 });
 
 server.listen(port, hostname, () => {
