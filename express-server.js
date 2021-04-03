bdcf4ec178f23fc61883598f3a04a423bc4aa540
Petr <solodov.petr@gmail.com>
2021-04-03 22:18:14 +0300

    Middleware usage, set cookie example

--- express-server.js
+++ express-server.js
@@ -6,18 +6,39 @@ const html = '<html lang="en"><body><h1 style="background: lemonchiffon">Express
 
 const HTML = fs.readFileSync(`${__dirname}/index.html`);
 
-// Middleware, do something on all the routes
+// Middleware, do something on all the routes - /public/css
 app.use('/public/css', express.static(__dirname + '/public/css'));
 
+// Middleware for all routes
+app.use('/', (req, res, next) => {
+    console.log('Requested url is:', req.url);
+    // set cookies
+    res.cookie('Express', 'Cookie');
+    next();
+});
 
-app.get('/', (request, response) => {
+// Middleware for specific route
+const myMiddleware = (req, res, next) => {
+    console.log('My middleware fires!');
+    const { id, model } = req.params;
+
+    if ( id && model ) {
+        console.log('ID:', id);
+        console.log('Model:', model);
+    }
+
+    next();
+};
+
+app.get('/', myMiddleware, (request, response) => {
     // response.send(HTML) // load the HTML
-    response.end(HTML)
+    response.end(HTML);
 })
 
 
+
 // Params
-app.get('/api/car/:model/:id', (request, response) => {
+app.get('/api/car/:model/:id', myMiddleware, (request, response) => {
     // get params from request - /api/car/sport/7
     const { id, model } = request.params;
 
